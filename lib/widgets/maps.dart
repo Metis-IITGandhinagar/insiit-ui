5b86add8ead8d98a4beca1dc49bc451c38c2728d
Advait <advaitandhale87@gmail.com>
2026-06-24 18:00:42 +0530

    Moved from mapbox to flutter_maps. Mapbox couldn't compile to sdk 36.

--- lib/widgets/maps.dart
+++ lib/widgets/maps.dart
@@ -31,6 +31,49 @@
 //       cameraOptions: camera,
 //     )
 
+
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart'; // Used here
+import 'package:latlong2/latlong.dart';       // Used here
+
+class MapPage extends StatefulWidget {
+  @override
+  _MapPageState createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  @override
+  Widget build(BuildContext context) {
+    String accessToken = const String.fromEnvironment("ACCESS_TOKEN");
+
+    // Mapbox Raster Tiles API URL
+    // You can swap 'streets-v11' with 'outdoors-v11', 'satellite-v9', etc.
+    final String mapboxUrl =
+        'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=$accessToken';
+
+    return Scaffold(
+      body: FlutterMap(
+        options: MapOptions(
+          initialCenter: LatLng(23.21049, 72.68475), // Notice: LatLng is (lat, lng)
+          initialZoom: 17,
+          initialRotation: 0, // Equivalent to bearing
+        ),
+        children: [
+          TileLayer(
+            urlTemplate: mapboxUrl,
+            additionalOptions: {
+              'accessToken': accessToken,
+            },
+            // Mapbox tiles require a 512px size for proper scaling on high-res screens
+            tileSize: 512,
+            zoomOffset: -1,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
         // FlutterMap(
         //   mapController: _mapController,
         //   options: MapOptions(
